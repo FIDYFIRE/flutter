406eb5fbb09e12f873fdeb5b93c342bcb33a7df3
FIDYFIRE <fidyfire@gmail.com>
2023-06-13 13:41:11 +0300

    first commit

---
+++ bin/mon_premier_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:mon_premier_flutter/mon_premier_flutter.dart' as mon_premier_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mon_premier_flutter.calculate()}!');
+}
---
+++ test/mon_premier_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:mon_premier_flutter/mon_premier_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
